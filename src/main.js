df5161a09242b9634aa82b112e8bbfec8e5188c0
Narathorn Noophum <6310210181@psu.ac.th>
2023-09-26 22:17:17 +0700

    Lab11

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-Vue.config.productionTip = false
-Vue.prototype.$EventBus = new Vue()
+Vue.use(VueAxios, axios);
+Vue.config.productionTip = false;
+Vue.prototype.$EventBus = new Vue();
+
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
