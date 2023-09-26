16860bf9654ff29fedefdd1d1148b53284e9d5c7
Narathorn Noophum <6310210181@psu.ac.th>
2023-09-26 21:25:26 +0700

    Lab10

---
+++ src/views/TableView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <Table />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Table from "../components/Table";
+
+export default {
+  components: {
+    Table,
+  },
+};
+</script>
+
+<style></style>
